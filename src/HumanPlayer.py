0b9cdb3ddfbe6a957ae53a00d31c5f59933d60cb
aboveyou00 <brandonyoyoslade@gmail.com>
2017-04-04 18:06:50 -0600

    feat(player): allow players to enter their names

--- src/HumanPlayer.py
+++ src/HumanPlayer.py
@@ -2,7 +2,7 @@ from Player import *
 
 class HumanPlayer(Player):
     def __init__(self, name):
-        self.name = name
+        self.name = input(name + ", enter your name: ") or name
     
     def makeMove(self, game):
         while True:
