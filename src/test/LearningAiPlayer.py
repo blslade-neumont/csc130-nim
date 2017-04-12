7310210922f56fb171885ce67360a57ee8fb544e
aboveyou00 <brandonyoyoslade@gmail.com>
2017-04-12 15:11:27 -0600

    test(learning-ai-player): add unit tests comparing learning AI and perfect AI

--- src/test/LearningAiPlayer.py
+++ src/test/LearningAiPlayer.py
@@ -1,29 +1,45 @@
 from unittest import TestCase
 from LearningAiPlayer import *
 from RandomAiPlayer import *
+from PerfectAiPlayer import *
 from SilentUserInterface import *
 from Game import *
 from Board import *
 
 class TestLearningAiPlayer(TestCase):
     def setUp(self):
-        self.silentUi = SilentUserInterface()
-        self.lp1 = LearningAiPlayer()
+        self.lp = LearningAiPlayer()
     
     def tearDown(self):
-        self.silentUi = None
-        self.lp1 = None
+        self.lp = None
     
     def test_train_canBeatRandomPlayer(self):
         lp2 = LearningAiPlayer()
-        self.lp1.train(lp2, iterations=1000, printProgress=False)
-        
-        rp = RandomAiPlayer()
+        self.lp.train(lp2, iterations=1000, printProgress=False)
+        self.assertGreaterEqual(self.countWins(self.lp, RandomAiPlayer()), 99)
+    
+    def test_train_beatsPerfectPlayerWhenFirst(self):
+        perfect = PerfectAiPlayer()
+        self.lp.train(perfect, iterations=1000, printProgress=False)
+        self.assertEqual(self.countWins(self.lp, perfect, swap=False), 100)
+    
+    def test_train_losesToPerfectPlayerWhenLast(self):
+        perfect = PerfectAiPlayer()
+        self.lp.train(perfect, iterations=1000, printProgress=False)
+        self.assertEqual(self.countWins(perfect, self.lp, swap=False), 100)
+    
+    def countWins(self, test, other, swap=True):
+        silentUi = SilentUserInterface()
         wins = 0
-        for n in range(100):
-            game = Game(self.silentUi)
-            game.start(Board([3, 5, 7]), self.lp1, rp)
-            if self.lp1.wonLastGame:
-                wins += 1
         
-        self.assertGreaterEqual(wins, 99)
+        p1 = test
+        p2 = other
+        for n in range(100):
+            game = Game(silentUi)
+            game.start(Board([3, 5, 7]), p1, p2)
+            if test.wonLastGame:
+                wins += 1
+            if swap:
+                p1, p2 = p2, p1
+        
+        return wins
